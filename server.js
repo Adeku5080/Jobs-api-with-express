dbf8a227a485a4c87b5a072c1014ba8dea01f36c
Ali Adeku <aliadeku.aam@gmail.com>
2022-12-30 23:48:24 +0100

    Add cors

--- server.js
+++ server.js
@@ -5,11 +5,27 @@ const authMiddleware = require("./src/middlewares/auth")
 const connect = require("./database/connect");
 require("dotenv").config();
 
+//security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const app = express();
 
 const PORT = 5000;
 
+app.set('trust proxy',1)
+app.use(rateLimiter({
+    windowMs : 15 * 60 * 1000,
+    max :100
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
 
 app.use("/api/v1/auth",  authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobRouter);
